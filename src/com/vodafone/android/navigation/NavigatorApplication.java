a8fb6e9edb8494df5b3f30c324d8f5a4a2a7745a
Christian Kullander <christian@kullander.nu>
2010-06-22 14:18:21 +0200

    Server now set to the Vodafone/Wayfinder OSS development server.

--- src/com/vodafone/android/navigation/NavigatorApplication.java
+++ src/com/vodafone/android/navigation/NavigatorApplication.java
@@ -152,7 +152,7 @@ public class NavigatorApplication extends Application implements
 
 	static {
 		CLIENT_ID = "wf-android-demo";
-		SERVER_URL = "[MY SERVER]";
+		SERVER_URL = "oss-xml.services.wayfinder.com";
 		SERVER_PORT = 80;
 	}
 
